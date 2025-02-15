79d892261b375c59e39c274982a919cf3b31eae8
Jared Gonzalez <al222310412@gmail.com>
2025-02-15 08:17:07 -0600

    Prueba amplify

--- src/App.js
+++ src/App.js
@@ -9,17 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-    <p>
-          Desarrollo Jared 
+        <p className='txt'>
+          Desarrollo Jared Alejandro Gonzalez Ortega
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
